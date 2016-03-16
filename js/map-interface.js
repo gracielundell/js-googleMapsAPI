a35d5f266503e17cf86910636acd8e8321175c6e
brooke hurford and gracie lundell <brookebhurford@gmail.com>
2016-03-16 11:18:17 -0700

    Show location on google map

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,37 +1,29 @@
 var apiKey = require("./../.env").apiKey;
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-
 $(document).ready(function(){
   $("form#addressForm").submit(function(event){
     var splitAddress = $("#address").val();
     var address = splitAddress.split(" ").join("+");
+    var lat;
+    var long;
     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + apiKey).then(function(results){
-      $('#map').text("The latitude of " + splitAddress + " is " + results.results[0].geometry.location.lat);
+      lat = results.results[0].geometry.location.lat;
+      long = results.results[0].geometry.location.lng;
+      $('.geometry').text("The latitude of " + splitAddress + " is " + lat + ". The longitude of " + splitAddress + " is " + long);
+      var userLatLng = new google.maps.LatLng(lat, long);
+      var myOptions = {
+        zoom : 16,
+        center : userLatLng,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+      };
+      var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
     });
     event.preventDefault();
-    console.log('hi');
   });
 });
 
 
-// $(document).ready(function(){
-//   $("#getHike").click(function(){
-//     var splitAddress = $("#address").val();
-//     var address = splitAddress.split(" ").join("+");
-//     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + apiKey).then(function(results){
-//       $('#map').text("The latitude of " + splitAddress + " is " + results.results[0].geometry.location.lat);
-//     });
-//     console.log('hi');
-//   });
-// });
-//
+
 // $(document).ready(function(){
 //   $("#getHike").click(function(){
 //     var splitAddress = $("#address").val();
