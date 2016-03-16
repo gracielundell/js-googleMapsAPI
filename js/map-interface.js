e937bf6c01999dad06e0c388bd80a694ff6cfa85
brooke hurford and gracie lundell <brookebhurford@gmail.com>
2016-03-16 10:39:13 -0700

    Get the results of the form to stick on the page

--- js/map-interface.js
+++ js/map-interface.js
@@ -8,14 +8,40 @@ function initMap() {
   });
 }
 
-
 $(document).ready(function(){
-  $("#getHike").click(function(){
+  $("form#addressForm").submit(function(event){
     var splitAddress = $("#address").val();
     var address = splitAddress.split(" ").join("+");
     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + apiKey).then(function(results){
-      debugger;
       $('#map').text("The latitude of " + splitAddress + " is " + results.results[0].geometry.location.lat);
     });
+    event.preventDefault();
+    console.log('hi');
   });
 });
+
+
+// $(document).ready(function(){
+//   $("#getHike").click(function(){
+//     var splitAddress = $("#address").val();
+//     var address = splitAddress.split(" ").join("+");
+//     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + apiKey).then(function(results){
+//       $('#map').text("The latitude of " + splitAddress + " is " + results.results[0].geometry.location.lat);
+//     });
+//     console.log('hi');
+//   });
+// });
+//
+// $(document).ready(function(){
+//   $("#getHike").click(function(){
+//     var splitAddress = $("#address").val();
+//     var address = splitAddress.split(" ").join("+");
+//     var lat = results.results[0].geometry.location.lat;
+//     var long = results.results[0].geometry.location.lng;
+//     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + "&key=" + apiKey).then(function(results){
+//       debugger;
+//       $('#map').text("The latitude of " + splitAddress + " is " + lat);
+//       $('#map').text("The longitude of " + splitAddress + " is " + long);
+//     });
+//   });
+// });
